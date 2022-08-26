e0013498e2146a86e41b6ae02c1f0314706ab992
Arnab Das <86297998+arnabdas18@users.noreply.github.com>
2022-08-26 18:02:10 +0530

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quae, at quia cupiditate nisi autem non repellat placeat cum. Laboriosam optio nulla ipsa nobis vel voluptas modi tenetur. Mollitia ratione ut rerum eveniet qui suscipit, necessitatibus ducimus sed nemo culpa vel ullam quia eligendi? Molestias quibusdam totam magni praesentium itaque.
+      </p>
+    </section>
+  )
+}
+
+export default About;
